7a1ed80299906cdc8a3066314adc31fc39c595ae
dfv91 <d4fv91@gmail.com>
2019-06-08 14:40:42 -0300

    implementando datos desde Json + helpers + eventos

--- mercadoreact/src/App.js
+++ mercadoreact/src/App.js
@@ -2,6 +2,9 @@ import React from 'react';
 import Header from './Components/Header'
 import Menu from './Components/Menu'
 //import './App.css';
+import productos from './productos.json'
+import Product from './Components/Product'
+
 
 class App extends React.Component {
 
@@ -11,20 +14,29 @@ class App extends React.Component {
       title : "MercadoReact",
       slogan : "Mucho mas que JS",
       //products : ["Música", "Deporte", "Electro", "Pesca", "Calzado Masculino"]
+      /*
       products : [
         { nombre: "Guitarra Criolla", precio : 1200, stock: 300, categoria : "Música" },
         { nombre: "Paleta Ping Pong x 2", precio : 350, stock: 650, categoria : "Deportes" },
         { nombre: "Microondas", precio : 8600, stock: 100, categoria : "Electro" },
-        { nombre: "Pelota Basket", precio : 960, stock: 225, categoria : "Deportes" }
-      ]
+        { nombre: "Pelota Basket", precio : 960, stock: 225, categoria : "Deportes" },
+        { nombre: "Marcadores Color", precio : 85, stock: 400, categoria : "Librería" }
+        ]
+        */
+        products : productos
     }
   }
 
   render(){
+    const losProductos = this.state.products.map(
+      (product, index)=> <Product item={product} key={index} />
+    )
+
     return (
     <div className="App">
       <Header title={this.state.title} slogan={this.state.slogan}/>
       <Menu links={this.state.products} />
+      <section className="row">{losProductos}</section>
     </div>
   );
 }
--- mercadoreact/src/Components/Menu.js
+++ mercadoreact/src/Components/Menu.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react'
+import slugify from '../helpers/String.js'
 
 class Menu extends Component {
   render(){
@@ -16,36 +17,35 @@ class Items extends Component{
   constructor(){
     super()
     this.state = {
-      categories : null
+      categories : [],
+      loaded : false
     }
   }
 
     componentDidMount(){
+
       this.setState({
-        categories : new Set (this.props.links.map(link => link.categoria))
+        categories : [ ...new Set ( this.props.links.map(link => link.Categoria ) ) ],
+        loaded : true
       })
     }
 
-  slugify(string) {
-  const a = 'àáäâãåăæçèéëêǵḧìíïîḿńǹñòóöôœøṕŕßśșțùúüûǘẃẍÿź·/_,:;'
-  const b = 'aaaaaaaaceeeeghiiiimnnnooooooprssstuuuuuwxyz------'
-  const p = new RegExp(a.split('').join('|'), 'g')
-  return string.toString().toLowerCase()
-    .replace(/\s+/g, '-') // Replace spaces with -
-    .replace(p, c => b.charAt(a.indexOf(c))) // Replace special characters
-    .replace(/&/g, '-and-') // Replace & with ‘and’
-    .replace(/[^\w-]+/g, '') // Remove all non-word characters
-    .replace(/--+/g, '-') // Replace multiple - with single -
-    .replace(/^-+/, '') // Trim - from start of text
-    .replace(/-+$/, '') // Trim - from end of text
-}
   render(){
-  return this.props.links.map(
-    (link, index) =>
-    <li className= "nav-item"key={index}>
-      <a className="nav-link" href={this.slugify(link.categoria)}>{link.categoria}</a>
-    </li>
-    )
+
+    if( !this.state.loaded ){
+      return <li>Cargando...</li>
+
+    } else {
+
+      return this.state.categories.map(
+        (link, index) =>
+        <li className= "nav-item" key={index}>
+          <a className="nav-link" href={slugify(link)}>{link}</a>
+        </li>
+        )
+    }
+
+
   }
 }
 
